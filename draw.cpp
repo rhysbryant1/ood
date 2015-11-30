42df73b67e4bbe22ad4f72371ac326fea99044eb
Rhys Bryant <rb00186@whale12.eps.surrey.ac.uk>
2015-11-30 11:58:59 +0000

    Finished creating structure

--- draw.cpp
+++ draw.cpp
@@ -7,7 +7,7 @@
 /*--------------------------------------------------------------*/
 
 
-/*--------------------------------------------------------------*/
+/*-------------------------------------*/
 // Default Constructor
 
 Command::Command()
@@ -16,7 +16,7 @@ Command::Command()
 
 }
 
-/*--------------------------------------------------------------*/
+/*------------------------------------*/
 // Constructor taking in the float value
 
 Command::Command(float value)
@@ -25,7 +25,7 @@ Command::Command(float value)
 
 }
 
-/*--------------------------------------------------------------*/
+/*------------------------------------*/
 // Destructor
 
 Command::~Command()
@@ -35,75 +35,166 @@ Command::~Command()
 }
 
 
+/*--------------------------------------------------------------*/
+/* Left Class */
+/*--------------------------------------------------------------*/
+
+/*-------------------------------------*/
+// Default Constructor
+
+Left::Left()
+{
+
+
+}
+
+/*-------------------------------------*/
+// Destructor
+
+Left::~Left()
+{
+
+
+}
+
+/*-------------------------------------*/
+// Draw function
+
+void Left::draw()
+{
+
+}
+
+
+/*--------------------------------------------------------------*/
+/* Right Class */
+/*--------------------------------------------------------------*/
+
+/*-------------------------------------*/
+// Default Constructor
+
+Right::Right()
+{
+
+
+}
+
+/*-------------------------------------*/
+// Destructor
+
+Right::~Right()
+{
+
+
+}
+
+/*-------------------------------------*/
+// Draw function
+
+void Right::draw()
+{
+
+}
+
+
+
+/*--------------------------------------------------------------*/
+/* Forward Class */
+/*--------------------------------------------------------------*/
+
+/*-------------------------------------*/
+// Default Constructor
+
+Forward::Forward()
+{
+
+
+}
+
+/*-------------------------------------*/
+// Destructor
+
+Forward::~Forward()
+{
+
+
+}
+
+/*-------------------------------------*/
+// Draw function
+
+void Forward::draw()
+{
+
+}
 
 
 
 
+/*--------------------------------------------------------------*/
+/* Jump Class */
+/*--------------------------------------------------------------*/
+
+/*-------------------------------------*/
+// Default Constructor
+
+Jump::Jump()
+{
+
+
+}
+
+/*-------------------------------------*/
+// Destructor
+
+Jump::~Jump()
+{
+
+
+}
+
+/*-------------------------------------*/
+// Draw function
+
+void Jump::draw()
+{
+
+}
 
 
 
 
+/*--------------------------------------------------------------*/
+/* Repeat Class */
+/*--------------------------------------------------------------*/
+
+/*-------------------------------------*/
+// Default Constructor
+
+Repeat::Reapeat()
+{
+
+
+}
+
+/*-------------------------------------*/
+// Destructor
+
+Repeat::~Repeat()
+{
+
+
+}
+
+/*-------------------------------------*/
+// Draw function
+
+void Repeat::draw()
+{
+
+}
 
 
 
 
-
-
-
-class Left: public Command{
-	public:
-		void Left();
-		void ~Left();
-		
-	void Left:: draw(){
-	/* Draw function for Left instruction*/
-	
-	
-	};
-};
-
-
-class Right: public Command{
-	public:
-		void Right();
-		void ~Right();
-		
-	void Right:: draw(){
-	/* Draw function for the Right instruction*/
-	
-	
-	
-	};
-};
-
-
-class Forward: public Command{
-	public:
-		void Forawrd();
-		void ~Forawrd();
-		
-	void Foward:: draw(){
-	/* Draw function for the Forward instruction*/
-	
-	
-	};
-};
-
-class Jump: public Command{
-	public:
-		void Forward();
-		void ~Forward();
-		
-	void Forward:: draw(){
-	/* Draw function for the Jump instruction */
-	
-	
-	};
-};
-
-
-class Repeat: public Command{
-
-
-};
 
